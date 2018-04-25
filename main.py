f679d34be855f86ae016a69753c6a25e23e08d29
Chris Jaros <cjaros1@att.net>
2018-04-24 19:03:21 -0500

    added new pages for preps and ingredients and lists all of them on each, added login functionality

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ class Inventory(db.Model):
     invLocation=db.Column(db.String(3))
     invDateAdded=db.Column(db.DateTime())
     invExpDate=db.Column(db.DateTime())
-    invEmpID=db.Column(db.String(120))
+    invEmpID=db.Column(db.String(120), db.ForeignKey(Employee.empID))
 
     def __init__(self,invID,invName,invIsPrepped,invLocation,invDateAdded,invExpDate,invEmpID):
         self.invID=invEmpID
@@ -73,11 +73,11 @@ class Preps(db.Model):
     prepID=db.Column(db.Integer,primary_key=True)
     prepName=db.Column(db.String(120))
     prepShelfLife=db.Column(db.Integer)
-    prepIngr1=db.Column(db.Integer)
-    prepIngr2=db.Column(db.Integer)
-    prepIngr3=db.Column(db.Integer)
-    prepIngr4=db.Column(db.Integer)
-    prepIngr5=db.Column(db.Integer)
+    prepIngr1=db.Column(db.Integer, db.ForeignKey(Ingredients.ingID))
+    prepIngr2=db.Column(db.Integer, db.ForeignKey(Ingredients.ingID))
+    prepIngr3=db.Column(db.Integer, db.ForeignKey(Ingredients.ingID))
+    prepIngr4=db.Column(db.Integer, db.ForeignKey(Ingredients.ingID))
+    prepIngr5=db.Column(db.Integer, db.ForeignKey(Ingredients.ingID))
 
     def __init__(self, prepID, prepName, prepShelfLife, prepIngr1, prepIngr2, prepIngr3, prepIngr4, prepIngr5):
         self.prepID=prepID
@@ -117,32 +117,100 @@ def clockout():
 def manager():
     return render_template('manager.html',title='Manager', error="")
 
+@app.route("/ingredients",methods=['POST','GET'])
+def ingredients():
+
+    ings=(Ingredients.query.all())
+    table="<table>"
+    table_row="<tr>"
+    table_row+="<th>Ingredient Number</th>"
+    table_row+="<th>Ingredient Name</th>"
+    table_row+="<th>Ingredient Shelf Life</th>"
+    table_row+="<th>Checked</th>"
+    table_row+="</tr>"
+    table+=table_row
+    for i in range(len(ings)):
+        table_row="<tr>"
+        table_row+="<td>"+str(ings[i].ingID)+"</td>"
+        table_row+="<td>"+str(ings[i].ingName)+"</td>"
+        table_row+="<td align='right'>"+str(ings[i].ingShelfLife)+" hours</td>"
+        table_row+="<td align='right'><input type='checkbox' name=ingid"+str(ings[i].ingID)+"</td>"
+        table_row+="</tr>"
+        table+=table_row
+    table+="</table>"
+
+    return render_template("ingredients.html", title="Ingredients List", table=table)
+
+@app.route("/preps",methods=['POST','GET'])
+def preps():
+
+    prepsList=(Preps.query.all())
+    table="<table border='4px'>"
+    table_row="<tr>"
+    table_row+="<th>Prep Number</th>"
+    table_row+="<th>Prep Name</th>"
+    table_row+="<th>Prep Ingredients</th>"
+    table_row+="<th>Prep Shelf Life</th>"
+    table_row+="<th>Checked</th>"
+    table_row+="</tr>"
+    table+=table_row
+    for i in range(len(prepsList)):
+
+        prepIngs=Ingredients.query.get(prepsList[i].prepIngr1).ingName
+        if prepsList[i].prepIngr2:
+            prepIngs+=", "+Ingredients.query.get(prepsList[i].prepIngr2).ingName
+        if prepsList[i].prepIngr3:
+            prepIngs+=", "+Ingredients.query.get(prepsList[i].prepIngr3).ingName
+        if prepsList[i].prepIngr4:
+            prepIngs+=", "+Ingredients.query.get(prepsList[i].prepIngr4).ingName
+        if prepsList[i].prepIngr5:
+            prepIngs+=", "+Ingredients.query.get(prepsList[i].prepIngr5).ingName
+        table_row="<tr>"
+        table_row+="<td>"+str(prepsList[i].prepID)+"</td>"
+        table_row+="<td>"+str(prepsList[i].prepName)+"</td>"
+        table_row+="<td>"+prepIngs+"</td>"
+        table_row+="<td align='right'>"+str(prepsList[i].prepShelfLife)+" hours</td>"
+        table_row+="<td align='right'><input type='checkbox' name=prepid"+str(prepsList[i].prepID)+"</td>"
+        table_row+="</tr>"
+        table+=table_row
+    table+="</table>"
+    return table
+
 @app.route("/getemployee", methods=['POST'])
 def getemployee():
     employee_id=request.form['empID']
     employee_password=request.form['password']
     employee=Employee.query.filter_by(empID=employee_id).first()
     employee_pos=""
-    print(employee)
     if employee_id=="" or employee==None:
-        return render_template('clockin.html',title='Clock In', error="Employee {} Not Found: Please make sure your employee ID you've entered is correct".format(employee_id))
+        return render_template('clockin.html',title='Clock In', employee_error="Employee {} Not Found: Please make sure your employee ID you've entered is correct".format(employee_id))
+    elif employee_password=="" or int(employee_password) != employee.empPassword:
+        print("form password: "+ employee_password)
+        print("db password: "+str(employee.empPassword))
+        return render_template('clockin.html',title='Clock In', password_error="Password incorrect, please enter the valid password")
 
     if employee.empPosition=="M":
         employee_pos="Manager"
         managerPerms=True
     elif employee.empPosition=="B":
         employee_pos="Baker"
+        managerPerms=False
     elif employee.empPosition=="G":
         employee_pos="General Manager"
         managerPerms=True
     elif employee.empPosition=="T":
         employee_pos="Trainer"
+        managerPerms=False
     else:
         employee_pos="Associate"
+        managerPerms=False
 
     #employee.empFName="William"
     #db.session.commit()
-    return "<h1>Welcome, {}: {} {}.</h1><p> You have clocked in at {}</p>".format(employee_pos, employee.empFName, employee.empLName, datetime.now().strftime('%H:%M:%S'))
+    welcome_msg="Welcome, {}: {} {}.".format(employee_pos, employee.empFName, employee.empLName)
+    clockin_msg="You have clocked in at {}".format(datetime.now().strftime('%H:%M:%S'))
+    print("Successful Clock In with employee_id: {} and employee_password: {} at time: {} Manager Permissions are: {}".format(employee_id,employee_password,datetime.now().strftime('%H:%M:%S'),managerPerms))
+    return render_template('menu.html', title='Menu', error='', welcome_msg=welcome_msg, clockin_msg=clockin_msg)
 
 if __name__ == '__main__':
     app.run()
