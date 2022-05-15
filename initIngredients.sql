17d443df7960522fef32748f03658abedb6dc669
cjaros1 <cjaros1@att.net>
2022-05-15 14:09:38 -0500

    initialized ingredients, preps, and removed inventory since the program adds/deletes from inventory.

---
+++ initIngredients.sql
@@ -0,0 +1,5 @@
+Insert Into ingredients(ingID,ingName,ingShelfLife)
+Values
+(1,'Lemons',72),
+(2,'Tomatoes',72),
+(3,'Avocados',24);
---
+++ initPreps.sql
@@ -0,0 +1,7 @@
+insert into preps(prepID, prepName, prepShelfLife,prepIngr1,prepIngr2,prepIngr3,prepIngr4,prepIngr5,prepIngr1Quant,prepIngr2Quant,prepIngr3Quant,prepIngr4Quant,prepIngr5Quant)
+VALUES
+(1,'Wedged Lemons',24,1,99,99,99,99,20,99,99,99,99),
+(2,'Sliced Tomatoes',24,2,99,99,99,99,10,99,99,99,99),
+(3,'Sliced Avocados',8,3,99,99,99,99,10,99,99,99,99),
+(4,'Sliced Lemons',24,1,99,99,99,99,10,99,99,99,99),
+(5,'Wedged Tomatoes',24,2,99,99,99,99,10,99,99,99,99);
