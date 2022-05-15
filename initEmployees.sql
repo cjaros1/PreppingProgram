b0370f1b7f06d26b232d1da7d8130503bb2b5498
cjaros1 <cjaros1@att.net>
2022-05-15 09:01:43 -0500

    creating .sql files to add initial values into database

---
+++ initEmployees.sql
@@ -0,0 +1,6 @@
+use database.db
+Insert into employees(empID,empFName,empLName,empPassword,empPosition,empDOB,empHireDate)
+Values
+(1,'William','Hartnell',1111,'G',11-23-1963,1-1-2022),
+(2,'Patrick','Troughton',2222,'A',10-29-1966,1-1-2022),
+(3,'Jon','Pertwee',3333,'A',06-21-1969,1-1-2022);
